f61bf79c32441375336876859c1cc9106c64b616
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-08 17:14:34 -0400

    changed

--- app.js
+++ app.js
@@ -46,11 +46,9 @@ app.use((error, req, res, next) => {
     .json({ message: error.message || "An unknown error ocurred!" })
 })
 
-const port = process.env.PORT || 5000
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.81lugfw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => app.listen(port, "0.0.0.0"))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err))
