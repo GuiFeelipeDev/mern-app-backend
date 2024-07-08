bdb5785613fed997b15cc0cb5c86c368d03e567a
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-08 16:48:16 -0400

    changed port

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.81lugfw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err))
