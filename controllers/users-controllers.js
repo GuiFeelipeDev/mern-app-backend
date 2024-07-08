bc5e543d24af45dab540c4f4ad4b76b7b3408277
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-08 17:51:47 -0400

    changed bcrypt

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,7 +2,7 @@ const HttpError = require("../models/http-error")
 const { validationResult } = require("express-validator")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 const getUsers = async (req, res, next) => {
   let users
